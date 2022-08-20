6bec757d4fa2ea02d8ac1ca912d24d7f96fbfb7b
Glisten**STAR** <75786284+GlistenSTAR@users.noreply.github.com>
2022-08-20 08:05:47 +0200

    add the layout for ticket-checker

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,128 @@
+<script>
+    import { page } from "$app/stores";
+    import logo from "../../assets/logo.jpg";
+</script>
+
+<header>
+    <div class="corner">
+        <a href="/">
+            <img src={logo} alt="SvelteKit" />
+            <h2><b>T</b>ickets <b>C</b>hecker</h2>
+        </a>
+    </div>
+
+    <nav>
+        <svg viewBox="0 0 2 3" aria-hidden="true">
+            <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+        </svg>
+        <ul>
+            <li class:active={$page.url.pathname === "/"}>
+                <a sveltekit:prefetch href="/">Home</a>
+            </li>
+            <li class:active={$page.url.pathname === "/release-check"}>
+                <a sveltekit:prefetch href="/release-check">Release-Checker</a>
+            </li>
+            <li class:active={$page.url.pathname === "/x-check"}>
+                <a sveltekit:prefetch href="/x-check">X-Checker</a>
+            </li>
+        </ul>
+        <svg viewBox="0 0 2 3" aria-hidden="true">
+            <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+        </svg>
+    </nav>
+</header>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-between;
+		width: 100%;
+    }
+
+    .corner a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+    }
+    .corner h2 {
+        font-size: 2em;
+        color: var(--blue);
+    }
+	.corner h2 b{
+		font-size: 1.2em;
+	}
+
+    .corner img {
+        margin-left: 20px;
+        width: 3em;
+        height: 3em;
+        object-fit: contain;
+		margin-right: 10px;
+    }
+
+    nav {
+        display: flex;
+        justify-content: center;
+        --background: rgba(255, 255, 255, 0.7);
+    }
+
+    svg {
+        width: 2em;
+        height: 3em;
+        display: block;
+    }
+
+    path {
+        fill: var(--background);
+    }
+
+    ul {
+        position: relative;
+        padding: 0;
+        margin: 0;
+        height: 3em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        list-style: none;
+        background-size: contain;
+		background-color: var(--background);
+    }
+
+    li {
+        position: relative;
+        height: 100%;
+    }
+
+    li.active::before {
+        --size: 10px;
+        content: "";
+        width: 0;
+        height: 0;
+        position: absolute;
+        top: 0;
+        left: calc(50% - var(--size));
+        border: var(--size) solid transparent;
+        border-top: var(--size) solid var(--blue);
+    }
+
+    nav a {
+        display: flex;
+        height: 100%;
+        align-items: center;
+        padding: 0 1em;
+        color: var(--heading-color);
+        font-weight: 700;
+        font-size: 0.8rem;
+        text-transform: uppercase;
+        letter-spacing: 0.1em;
+        text-decoration: none;
+        transition: color 0.2s linear;
+    }
+
+    a:hover {
+        text-decoration: none;
+    }
+</style>
---
+++ src/routes/release-check/+page.svelte
@@ -0,0 +1,234 @@
+<script>
+    import Select from "svelte-select";
+    import Switch from "$lib/common/Switch.svelte";
+
+    import etrix from "../../assets/site-logos/etrix.png";
+    import eventbrite from "../../assets/site-logos/eventbrite.png";
+    import frontgate from "../../assets/site-logos/frontgate.png";
+    import seetickets from "../../assets/site-logos/seetickets.png";
+    import showclix from "../../assets/site-logos/showclix.png";
+    import ticketweb from "../../assets/site-logos/ticketweb.png";
+    import prekindle from "../../assets/site-logos/prekindle.png";
+    import bigtickets from "../../assets/site-logos/bigtickets.svg";
+
+    import { SettingsIcon, PlayIcon } from "svelte-feather-icons";
+
+    let active_leftSidebar = "etrix";
+
+    let items = [
+        { value: "capmonster", label: "Capmonster" },
+        { value: "anticaptcha", label: "AntiCaptcha" },
+    ];
+
+    let value = { value: "capmonster", label: "Capmonster" };
+
+    let sliderValue;
+
+    const clickTab = (value) => {
+        active_leftSidebar = value;
+    };
+    const handleSelect = (event) => {
+        console.log("selected item", event.detail);
+    };
+</script>
+
+<svelte:head>
+    <title>Release-check</title>
+    <meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="content">
+    <div class="left_sidebar">
+        <ul>
+            <li
+                class={active_leftSidebar === "etrix" ? "active" : ""}
+                on:click={() => clickTab("etrix")}
+            >
+                <img src={etrix} alt="site-logo" />
+                <p>Etrix</p>
+            </li>
+            <li
+                class={active_leftSidebar === "eventbrite" ? "active" : ""}
+                on:click={() => clickTab("eventbrite")}
+            >
+                <img src={eventbrite} alt="site-logo" />
+                <p>Eventbrite</p>
+            </li>
+            <li
+                class={active_leftSidebar === "bigtickets" ? "active" : ""}
+                on:click={() => clickTab("bigtickets")}
+            >
+                <img src={bigtickets} alt="site-logo" />
+                <p>BigTicket</p>
+            </li>
+            <li
+                class={active_leftSidebar === "frontgate" ? "active" : ""}
+                on:click={() => clickTab("frontgate")}
+            >
+                <img src={frontgate} alt="site-logo" />
+                <p>FrontGate</p>
+            </li>
+            <li
+                class={active_leftSidebar === "ticketweb" ? "active" : ""}
+                on:click={() => clickTab("ticketweb")}
+            >
+                <img src={ticketweb} alt="site-logo" />
+                <p>TicketWeb</p>
+            </li>
+            <li
+                class={active_leftSidebar === "seetickets" ? "active" : ""}
+                on:click={() => clickTab("seetickets")}
+            >
+                <img src={seetickets} alt="site-logo" />
+                <p>SeeTickets</p>
+            </li>
+            <li
+                class={active_leftSidebar === "showclix" ? "active" : ""}
+                on:click={() => clickTab("showclix")}
+            >
+                <img src={showclix} alt="site-logo" />
+                <p>ShowClix</p>
+            </li>
+            <li
+                class={active_leftSidebar === "prekindle" ? "active" : ""}
+                on:click={() => clickTab("prekindle")}
+            >
+                <img src={prekindle} alt="site-logo" />
+                <p>Prekindle</p>
+            </li>
+        </ul>
+    </div>
+    <div class="right_content">
+        <div class="mini_setting">
+            <div style="min-width: 250px" class="captcha">
+                <Select
+                    {items}
+                    {value}
+                    on:select={handleSelect}
+                    inputStyles="font-size: 18px;"
+                />
+            </div>
+            <div style="margin-left: 20px; display: flex">
+                <Switch
+                    bind:value={sliderValue}
+                    label="Proxy: "
+                    fontSize={18}
+                    design="slider"
+                />
+                {#if active_leftSidebar === "etrix"}
+                    <Switch
+                        bind:value={sliderValue}
+                        label="Decrease: "
+                        fontSize={18}
+                        design="slider"
+                    />
+                {/if}
+            </div>
+            <div class="buttons">
+                <button class="icon_button">
+                    <SettingsIcon />
+                </button>
+                <button class="icon_button">
+                    <PlayIcon />
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .content {
+        width: 100%;
+        background-color: rgba(255, 255, 255, 0.3);
+        border-radius: 20px;
+        padding-left: 5px;
+        padding-right: 5px;
+
+        display: flex;
+        min-height: 65vh;
+    }
+    .left_sidebar {
+        width: 20%;
+        padding-right: 20px;
+        padding: 20px;
+        padding-left: 0px;
+        min-width: 280px;
+    }
+    .left_sidebar li {
+        list-style: none;
+        font-size: 22px;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        margin-top: 4px;
+        margin-bottom: 4px;
+        cursor: pointer;
+        color: var (--heading-color);
+        padding-left: 15px;
+
+        display: flex;
+        align-items: center;
+    }
+    .left_sidebar ul {
+        padding-left: 15px;
+    }
+    .left_sidebar li:hover {
+        background-color: #55cdf8;
+        border-radius: 8px;
+    }
+    .left_sidebar li.active {
+        background-color: #55cdf8;
+        border-radius: 8px;
+    }
+    .left_sidebar li img {
+        width: 25px;
+        height: 25px;
+    }
+    .left_sidebar li p {
+        margin-left: 7px;
+        margin-top: 0px;
+        margin-bottom: 0px;
+    }
+    .left_sidebar li:hover p,
+    .left_sidebar li.active p {
+        color: white;
+    }
+
+    .right_content {
+        width: 80%;
+        border-left: 2px solid rgb(177, 176, 176);
+        padding: 20px;
+    }
+
+    .right_content .mini_setting {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .right_content .mini_setting .captcha {
+        --itemHoverBG: #55cdf8;
+        --itemIsActiveBG: #55cdf8;
+    }
+
+    .buttons {
+        display: flex;
+    }
+    .icon_button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 7px;
+        margin-left: 10px;
+        color: #55cdf8;
+        border-radius: 8px;
+        border-color: #55cdf8;
+        border-style: solid;
+    }
+    .icon_button:hover {
+        background-color: #55cdf8;
+        color: white;
+    }
+    .icon_button:active {
+        background-color: #1bbdf8;
+        color: grey;
+    }
+</style>
