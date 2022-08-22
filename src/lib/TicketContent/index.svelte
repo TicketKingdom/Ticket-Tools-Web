167edd15786075f54111d23ea29627c08b29402e
Glisten**STAR** <75786284+GlistenSTAR@users.noreply.github.com>
2022-08-22 19:58:36 +0200

    modify ui for datatables

--- src/lib/TicketContent/index.svelte
+++ src/lib/TicketContent/index.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { PlusIcon } from "svelte-feather-icons";
+    import { PlusIcon, PlayIcon, EditIcon, DeleteIcon } from "svelte-feather-icons";
     import { Datatable, rows } from "$lib/common/SimpleDatatables";
 
     export let site_name = "";
@@ -24,7 +24,36 @@
         },
     };
 
-    let data = [{index:"0"}, {index:"1"}];
+    let data = [
+        {
+            index: "0",
+            url: "https://event.etix.com/ticket/online/performanceSale.do?specifiedYearMonth=true&performance_id=3101270&country=US&language=en&method=restoreToken",
+            name: "BILLY STRINGS",
+            date: "2022-08-12",
+            result: 78,
+            interval: 259200,
+            notication: 5000,
+            row: 1,
+            added_date: "2022-08-15",
+            last_check_date: "2022-08-12",
+            cart_time: true,
+            presale_pw: "",
+        },
+        {
+            index: "1",
+            url: "https://event.etix.com/ticket/online/performanceSale.do?specifiedYearMonth=true&performance_id=3101270&country=US&language=en&method=restoreToken",
+            name: "BILLY STRINGS",
+            date: "2022-08-12",
+            result: 78,
+            interval: 259200,
+            notication: 5000,
+            row: 1,
+            added_date: "2022-08-15",
+            last_check_date: "2022-08-12",
+            cart_time: true,
+            presale_pw: "",
+        },
+    ];
 </script>
 
 <div class="ticket_content">
@@ -37,23 +66,44 @@
         <Datatable {settings} {data}>
             <thead>
                 <th width="3%" data-key="index">#</th>
-                <th width="5%">Event Name</th>
-                <th width="20%">Url</th>
-                <th width="5%">Date</th>
-                <th width="10%">Result</th>
+                <th width="5%" data-key="name">Event Name</th>
+                <th width="15%">Url</th>
+                <th width="5%" data-key="date">Date</th>
+                <th width="3%" data-key="result">Result</th>
                 <th width="5%">Interval</th>
-                <th width="5%">Notification</th>
-                <th width="5%">Row</th>
-                <th width="5%">Added On</th>
-                <th width="5%">Last Check</th>
-                <th width="5%">Cart Timer</th>
-                <th width="10%">Presale Password</th>
+                <th width="5%" data-key="notication">Notification</th>
+                <th data-key="row">Row</th>
+                <th width="5%" data-key="added_date">Added On</th>
+                <th width="5%" data-key="last_check_date">Last Check</th>
+                <th width="3%">Cart <br />Timer</th>
+                <th data-key="presale_pw">Presale Password</th>
+                <th>actions</th>
             </thead>
             <tbody>
                 {#each $rows as row, index}
                     <tr>
                         <td>{index + 1}</td>
-                        <td>{index + 1}</td>
+                        <td>{row.name}</td>
+                        <td
+                            style=" overflow: hidden;
+                                    text-overflow: ellipsis; "
+                            class="truncation_text"
+                        >
+                            {row.url}</td>
+                        <td>{row.date}</td>
+                        <td>{row.result}</td>
+                        <td>{row.interval}</td>
+                        <td>{row.notication}</td>
+                        <td>{row.row}</td>
+                        <td>{row.added_date}</td>
+                        <td>{row.last_check_date}</td>
+                        <td>{row.cart_time}</td>
+                        <td>{row.presale_pw}</td>
+                        <td>
+                            <PlayIcon size="1.5x" class="primary"/>
+                            <EditIcon size="1.5x" class="success"/>
+                            <DeleteIcon size="1.5x" class="danger"/>
+                        </td>
                     </tr>
                 {/each}
             </tbody>
@@ -100,12 +150,12 @@
     }
     th {
         text-align: left;
-        font-size: 14px;
+        font-size: 12px;
         color: grey;
         cursor: pointer;
     }
     td {
-        text-align: left;
+        text-align: center;
         padding: 4px 0;
     }
 </style>
